491a3af5848eb792b99873ee5490b3509c694bd6
Scheck Levente <schecklevente@Schecks-MacBook-Pro.local>
2017-10-09 14:03:45 +0300

    Demo

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/hello', function(request, response) {
+  response.end('Hello heroku');
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
