dc90b2cfaef5a05833526ae1ca002bfcca85a8a9
Scheck Levente <schecklevente@Schecks-MacBook-Pro.local>
2017-10-09 16:41:36 +0300

    Demo

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.get('/hello', function(request, response) {
 
 app.get('/db', function (request, response) {
   //var pool = new pg.Pool();
-
+  //https://devcenter.heroku.com/articles/getting-started-with-nodejs#provision-a-database
+  // psql -h localhost
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
@@ -32,19 +33,7 @@ app.get('/db', function (request, response) {
        { response.render('pages/db', {results: result.rows} ); }
     });
   });
-  
-  // pool.connect(process.env.DATABASE_URL, function(err, client, done) {
-  //   response.end('db');
-  //   // client.query('SELECT * FROM test_table', function(err, result) {
-  //   //   done();
-  //   //   if (err)
-  //   //    { console.error(err); response.send("Error " + err); }
-  //   //   else
-  //   //    //{ response.render('pages/db', {results: result.rows} ); }
-  //   //    { response.end(JSON.stringify(result.rows) ); }
-  //   // });
-  // });
-  // pool.end()
+
 });
 
 
